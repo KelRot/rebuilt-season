704c5c1645827619fa96c6023f54da9afb2a7961
Zeynep Afra Yilmaz <zeynepafrayilmaz07@gmail.com>
2026-01-15 13:50:09 +0300

    added build constants

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "rebuilt-season";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "e0dd0fd019e09bf6b01116cda6ee8e4571eaeeb5";
+  public static final String GIT_DATE = "2026-01-15 10:11:18 GMT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-15 10:47:45 GMT";
+  public static final long BUILD_UNIX_TIME = 1768474065843L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
